69b092494669f2a67bd2991c30b83aa77fd3fd31
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-12-11 08:40:30 -0300

    Creating turtle controller

--- turtlesim_catch_them_all/setup.py
+++ turtlesim_catch_them_all/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "turtlesim_controller = turtlesim_catch_them_all.turtle_controller:main"
         ],
     },
 )
